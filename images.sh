4b8845b3d67dda2b3e35b8d4a4ec4781cca74aaa
rakeshyadavj3 <71177481+rakeshyadavj3@users.noreply.github.com>
2022-12-26 05:02:54 +0530

    Update images.sh

--- images.sh
+++ images.sh
@@ -1,7 +1,7 @@
-count=$(curl -s -u admin:nexus -X GET http://35.74.253.177:8081/service/rest/v1/search?repository=${1} | jq '.items[].name' | wc -l)
+count=$(curl -s -u admin:nexus -X GET http://35.74.253.177:8081/service/rest/v1/search?repository=docker-hosted | jq '.items[].name' | wc -l)
 if [ $count -eq 0 ]
 then
    echo "no-images"
 else
-   curl -s -u admin:nexus -X GET http://35.74.253.177:8081/service/rest/v1/search?repository=${1} | jq '.items[] | .name + ":" + .version' | sed 's/\"//g'
+   curl -s -u admin:nexus -X GET http://35.74.253.177:8081/service/rest/v1/search?repository=docker-hosted | jq '.items[] | .name + ":" + .version' | sed 's/\"//g'
 fi
