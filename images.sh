e3f17553be4ee0524cd969572f91d1bbc736f6c3
rakeshyadavj3 <71177481+rakeshyadavj3@users.noreply.github.com>
2022-12-26 04:26:30 +0530

    Update images.sh

--- images.sh
+++ images.sh
@@ -3,5 +3,5 @@ if [ $count -eq 0 ]
 then
    echo "no-images"
 else
-   curl -s -u admin:nexus -X GET http://35.74.253.177:8081/service/rest/v1/search?repository=${1} | jq '.items[] | .name + ":" + .version' | sed 's/"//g'
+   curl -s -u admin:nexus -X GET http://35.74.253.177:8081/service/rest/v1/search?repository=${1} | jq '.items[] | .name + ":" + .version' | sed 's/\"//g'
 fi
